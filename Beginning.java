d9c75383b7f1a317ba1deeb84f79ebd9db3e1d1e
NKostin <29-03@rambler.ru>
2016-12-02 12:06:05 +0200

    Update Beginning.java

--- Beginning.java
+++ Beginning.java
@@ -5,11 +5,8 @@ public class Beginning extends Thread {
 	public static void main(String args[]) throws IOException{
 		Module modules = new Module();
 		Reader read = new Reader(modules);
-		//Thread readThread = new Thread(read);
-		//Thread modulesThread = new Thread(modules);
 		read.start();
 		modules.start();
-	//	modulesThread.start();
 	}
 
 }
