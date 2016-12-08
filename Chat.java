c4e8790d5d81b6361739f7825e5ce227b267d6bd
NKostin <29-03@rambler.ru>
2016-12-08 10:20:24 +0200

    Add files via upload

---
+++ Chat.java
@@ -0,0 +1,45 @@
+package serverSimulation;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Chat extends Thread {
+	 Scanner in = new Scanner(System.in);
+	 Encryption encryp3 = new Encryption();
+	 boolean exit = true;
+	 public  void run(){
+		 System.out.println("Добропожаловать в чат!");
+		 System.out.println("для выхода из чата введите !exit");
+		 while(exit){
+			 scanString();
+			 try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 }
+		 System.out.print("Досвидания!");
+	 }
+	 public  void scanString(){
+		 String exit="!exit";
+			 System.out.print(">");
+			 String scan = in.nextLine();
+			 if(scan.equals(exit)){
+				 this.exit=false;
+			 } else {
+			 
+				 String shifrString = encryp3.shifr(scan);
+				 try {
+					 FileWriter writerChatString = new FileWriter("src/outputStream",false);
+					 writerChatString.write(shifrString);
+					 writerChatString.write(System.lineSeparator());
+					 writerChatString.flush();
+				 } catch (IOException e) {
+					 // TODO Auto-generated catch block
+					 e.printStackTrace();
+				 }
+			 }
+	 }
+}
